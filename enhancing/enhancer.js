b2ad1d67ed94a9ad436781ec7c0276d3555ae81b
Mark Ambro <wowmarkambro@gmail.com>
2020-09-08 20:59:38 -0500

    👌 IMPROVE: added tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,61 @@
 module.exports = {
-  success,
-  fail,
-  repair,
-  get,
+	success,
+	fail,
+	repair,
+	get,
 };
 
+// item {
+// name
+// enhancement 0-20,
+// * success: increments, max 20, no affect on durability
+// ! fail:>15 = durability -5 || >15 = durability-10
+// ! fail: >16 = enhancement -1
+// durability: 0-100,
+// }
+function repair(item) {
+	// durability to 100
+	item.durability = 100;
+	return { ...item };
+}
+
 function success(item) {
-  return { ...item };
+	const newItem = { ...item };
+	const { enhancement } = newItem;
+	// enhancement 0-20,
+	// * success: increments enhancement, max 20, no affect on durabilit
+	if (enhancement < 20) {
+		newItem.enhancement++;
+	}
+	return { ...newItem };
 }
 
 function fail(item) {
-  return { ...item };
-}
+	const newItem = { ...item };
 
-function repair(item) {
-  return { ...item };
+	if (item.enhancement > 16) {
+		newItem.enhancement = item.enhancement - 1;
+	}
+
+	if (item.enhancement >= 15) {
+		// let check that we dont go NEG
+		if (item.durability >= 10) {
+			newItem.durability = item.durability - 10;
+		} else {
+			// else, if uner 10 we return a ZERO
+			newItem.durability = 0;
+		}
+	} else {
+		if (item.durability >= 5) {
+			newItem.durability = item.durability - 5;
+		} else {
+			// else, if uner 5 we return a ZERO
+			newItem.durability = 0;
+		}
+	}
+	return { ...newItem };
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,66 @@
-const enhancer = require('./enhancer.js');
+const en = require('./enhancer.js');
 // test away!
+
+describe('🧪 Enhancer unit test', () => {
+	it('Runs Function Repairs', () => {
+		const rustyItem = {
+			name: 'bronze sword',
+			enhancement: 10, // 0-20
+			durability: 50, //0-100
+		};
+
+		expect(en.repair(rustyItem)).toEqual({
+			name: 'bronze sword',
+			enhancement: 10, // 0-20
+			durability: 100, //0-100
+		});
+	});
+
+	it('Runs Function Success', () => {
+		const rustyItem = {
+			name: 'bronze sword',
+			enhancement: 20, // 0-20
+			durability: 50, //0-100
+		};
+
+		expect(en.success(rustyItem)).toEqual({
+			name: 'bronze sword',
+			enhancement: 20, // 0-20
+			durability: 50, //0-100
+		});
+	});
+
+	it('Runs Function Fail', () => {
+		const RI1 = {
+			name: 'bronze sword',
+			enhancement: 5, // 0-20
+			durability: 50, //0-100
+		};
+		const RI2 = {
+			name: 'bronze sword',
+			enhancement: 15, // 0-20
+			durability: 10, //0-100
+		};
+		const RI3 = {
+			name: 'bronze sword',
+			enhancement: 17, // 0-20
+			durability: 50, //0-100
+		};
+
+		expect(en.fail(RI1)).toEqual({
+			name: 'bronze sword',
+			enhancement: 5, // 0-20
+			durability: 45, //0-100
+		});
+		expect(en.fail(RI2)).toEqual({
+			name: 'bronze sword',
+			enhancement: 15, // 0-20
+			durability: 0, //0-100
+		});
+		expect(en.fail(RI3)).toEqual({
+			name: 'bronze sword',
+			enhancement: 16, // 0-20
+			durability: 40, //0-100
+		});
+	});
+});
